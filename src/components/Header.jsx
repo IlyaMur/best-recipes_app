b014728abae0197e34eef0df1680d76ee279d741
Ilya Mur <ilyamur@hotmail.com>
2021-09-08 15:06:40 +0300

    add Header && Footer + react router

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+export function Header() {
+  return (
+    <nav className="green darken-1">
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">
+          Toy-Shop
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a
+              href="https://github.com/IlyaMur/react_shop"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
