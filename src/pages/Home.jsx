aaafd0e67eab811e80a4eb816d90afbc0238ffcb
Ilya Mur <ilyamur@hotmail.com>
2021-09-09 17:31:25 +0300

    add search by filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,33 @@ import React, { useState, useEffect } from "react";
 import { getAllCategories } from "../api";
 import { Preloader } from "../components/Preloader";
 import { CategoryList } from "../components/CategoryList";
+import { Search } from "../components/Search";
 
 function Home() {
   const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+  };
 
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data.categories);
+      setFilteredCatalog(data.categories);
     });
   }, []);
 
-  return <>{!catalog ? <Preloader /> : <CategoryList catalog={catalog} />}</>;
+  return (
+    <>
+      <Search cb={handleSearch} />
+      {!catalog ? <Preloader /> : <CategoryList catalog={filteredCatalog} />}
+    </>
+  );
 }
 
 export { Home };
