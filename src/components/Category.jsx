d5b15f9fb95da919a946207d8f7e6e06402f5257
Ilya Mur <ilyamur@hotmail.com>
2021-09-09 17:09:53 +0300

    add recipe page&&ingredients table

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getFilteredCategory } from "../api";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { Preloader } from "../components/Preloader";
 
 import { MealList } from "../components/MealList";
@@ -8,12 +8,20 @@ import { MealList } from "../components/MealList";
 function Category() {
   const { name } = useParams();
   const [meals, setMeals] = useState();
+  const { goBack } = useHistory();
 
   useEffect(() => {
     getFilteredCategory(name).then((data) => setMeals(data.meals));
   }, [name]);
 
-  return <>{!meals ? <Preloader /> : <MealList meals={meals} />}</>;
+  return (
+    <>
+      <button className="btn" onClick={goBack}>
+        Go Back
+      </button>
+      {!meals ? <Preloader /> : <MealList meals={meals} />}
+    </>
+  );
 }
 
 export { Category };
